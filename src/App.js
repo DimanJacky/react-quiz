54cb6e7d0703cff4aa9a99664d15c9c539576f9c
DimanJacky <maslov-mda@yandex.ru>
2020-06-09 13:12:14 +0300

    project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import Layout from "./hoc/Layout/Layout";
 
 class App extends Component {
   render() {
     return (
-        <Layout>
-
-            <div style={{width: 400, border: '1px solid black'}}>
-                <h1>Layout works</h1>
-            </div>
-        </Layout>
+      <div className="App">
+        <h1>Hello React</h1>
+      </div>
     );
   }
 }
